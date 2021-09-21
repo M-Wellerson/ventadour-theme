6237098bfee81d01c0fae168988f2e3d6956e850
brunocriacoes <br.rafael@outlook.com>
2021-09-21 11:34:21 -0300

    fix

--- single-product.php
+++ single-product.php
@@ -11,34 +11,56 @@
 </div>
 
 <div class="wrapper ventes">
-    <div class="single-grid-2">
+    <div class="flex between">
         <div>
-            <strong class="home-quick-view-title">VENTADOUR</strong>
-            <span class="home-quick-view-subtitle">GAZEUSE</span>
+            <strong class="title-big">O’COOL</strong><br>
+            <span class="title-sub">GAZEUSE</span>
             <div class="home-quick-view-image">
                 <span style="background-color: #FDE17F;"></span>
                 <img src="<?= get_template_directory_uri() ?>/assets/images/zoo.png">
             </div>
         </div>
-        <div class="banner-padding-left">
-            <strong class="home-quick-view-preco">32,50 € </strong>
-            <span class="home-quick-view-add" style="background-color: #FDE17F;">ajouter au panier</span>
-            <select class="home-quick-view-select">
-                <option>Gazeuse</option>
-            </select>
-            <select class="home-quick-view-select">
-                <option>3 x PACKS DE 6</option>
-            </select>
-            <a href="" class="home-quick-view-link">INFORMATIONS NUTRICIONELLES</a>
-            <p class="home-quick-view-description">
-                <b>100% naturelles, garanties zéro nitrate!</b>
-                Une pure eau minérale naturelle,
-                venue des volcans d’ardèche! !
-            </p>
-            <br>
-            <img src="<?= get_template_directory_uri() ?>/assets/images/logos.png" width="100%">
+        <div>
+            <div class="box-single">
+                <div class="mb-10">
+                    <strong class="text c-gray-300">75,26 € </strong> 
+                    <strong class="text ml-10">52,50 €</strong> 
+                    <b class="text red-900 c-gray-50 single-discout ml-10">- 30%</b>  
+                </div>
+                <span class="home-quick-view-add" style="background-color: #FDE17F;">ajouter au panier</span>
+                <select class="home-quick-view-select text">
+                    <option>Gazeuse</option>
+                </select>
+                <select class="home-quick-view-select text mb-10">
+                    <option>3 x PACKS DE 6</option>
+                </select>
+                <a href="" class="home-quick-view-link text-small mb-10">INFORMATIONS NUTRICIONELLES</a>
+                <div class="more-detail">
+                    <img src="<?= get_template_directory_uri() ?>/assets/icons/sum.png">
+                    <span class="text">RELAXANTE</span>
+                </div>
+                <div class="more-detail">
+                    <img src="<?= get_template_directory_uri() ?>/assets/icons/erva.png">
+                    <span class="text">40 MG DE CBD PAR CANETTE</span>
+                </div>
+                <div class="more-detail mb-10">
+                    <img src="<?= get_template_directory_uri() ?>/assets/icons/erva.png">
+                    <span class="text">PAS STUPPEFIANTE</span>
+                </div>
+                <div class="hr mb-10"></div>
+                <div class="flex between">
+                    <div>
+                        <a href="#" class="more-link text-small">je suis </a> <br>
+                        <a href="#" class="more-link text-small">professionnel</a> <br>
+                    </div>
+                    <div>
+                        <a href="#" class="more-link text-small">FAQ</a> <br>
+                        <a href="#" class="more-link text-small">la LIVRAISON</a> <br>
+                        <a href="#" class="more-link text-small">moyens de paiement</a> <br>
+                    </div>
+                </div>
+            </div>
         </div>
-        <span onclick="close_quick_view()" class="quick-close">x</span>
     </div>
 </div>
 
@@ -57,37 +79,40 @@
 <div class="wrapper" style="margin-bottom: 35px;">
     <div class="single-grid-2">
         <div>
-            <h4>QUE EST CE QUE LE CBD ?</h4>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing
-                labore et dolore magna aliqua. Ut enim ad minim veniam,
+            <h4 class="title mb-10">QUE EST CE QUE LE CBD ?</h4>
+            <p class="text mb-30">
+                <b>Lorem ipsum dolor sit amet, consectetur adipiscing
+                labore et dolore magna aliqua. </b> Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi
                 ut aliquip ex ea commodo consequat.
             </p>
-            <span class="single-sitacao">
+            <span class="single-sitacao text-small mb-30">
                 Enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
             </span>
-            <span class="single-list">
+            <div class="hr"></div>
+            <span class="single-list text-small">
                 <img src="<?= get_template_directory_uri() ?>/assets/icons/securyt.png">
                 <span>
                     Veniam, quis nostrud exercitation ullamco
                     laboris nisi ut aliquip commodo consequat.
                 </span>
             </span>
-            <span class="single-list">
+            <div class="hr"></div>
+            <span class="single-list text-small">
                 <img src="<?= get_template_directory_uri() ?>/assets/icons/sum.png">
                 <span>
                     Enim ad minim veniam, quis nostrud exercitation
                     ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </span>
             </span>
+            <div class="hr"></div>
         </div>
     </div>
 </div>
 
 <div class="wrapper" style="margin-bottom: 100px;">
-    <h4>AUTRES BOISSONS</h4>
+    <h4 class="title">AUTRES BOISSONS</h4>
 </div>
 <?php include('includes/slider-home.php'); ?>
 
@@ -102,6 +127,7 @@
                     à partir de 90€
                 </span>
             </div>
+            <div class="track-space"></div>
             <div class="track-item">
                 <img src="<?= get_template_directory_uri() ?>/assets/icons/lock.png">
                 <span>
@@ -109,6 +135,7 @@
                     sécurisé
                 </span>
             </div>
+            <div class="track-space"></div>
             <div class="track-item">
                 <img src="<?= get_template_directory_uri() ?>/assets/icons/comment.png">
                 <span>
