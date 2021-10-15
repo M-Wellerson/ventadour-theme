10e90706a21a537d6f2bf395efc19957223f7edb
brunocriacoes <br.rafael@outlook.com>
2021-10-15 11:54:06 -0300

    fix

--- includes/actions.php
+++ includes/actions.php
@@ -1,20 +1,44 @@
 <?php
 
-// $args = array(
-//     81 => array(
-//         'product_id' => 1543,
-//         'quantity' => 3,
-//     ),
-//     84 => array(
-//         'product_id' => 1386,
-//         'quantity' => 1,
-//         'variation_id' => 3535,
-//         'attributes' => array(
-//             'attribute_pa_attribute-1' => 'value-1b',
-//             'attribute_pa_attribute-2' => 'value-2b',
-//         ),
 
+
+// $args = array(
+//     4 => array(
+//         'product_id' => 167,
+//         'quantity' => 2,
+//     ),
+//     5 => array(
+//         'product_id' => 169,
+//         'quantity' => 2,
 //     )
 // );
 
-// WC_PB()->cart->add_bundle_to_cart( 183 , 1, $args );
+// $teste = WC_PB()->cart->add_bundle_to_cart(192, 2, $args);
+
+// var_dump($teste);
+
+// $r = WC_PB_DB::query_bundled_items( array(
+//     'bundle_id' => array( 192 )
+// ) );
+
+// var_dump($r);
+
+
+// $r = WC_PB_DB::query_bundled_items(array(
+//     'return'    => 'id=>product_id',
+//     'bundle_id' => array(173)
+// ));
+
+// // $r = WC_PB_DB::get_bundled_item( 173 );
+
+// $args = array(
+//     'post_type'      => 'product',
+//     'posts_per_page' => 10,
+//     // 'product_cat'    => 'hoodies'
+// );
+
+// $loop = new WP_Query( $args );
+
+
+
+// var_dump( $loop );
