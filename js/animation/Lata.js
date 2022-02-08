f681bbf2a6afdafc0430b9f20d25f0180833d40f
brunocriacoes <br.rafael@outlook.com>
2022-02-08 10:24:07 -0300

    fix

--- js/animation/Lata.js
+++ js/animation/Lata.js
@@ -152,7 +152,6 @@ export default class Lata {
     }
 
     resize() {
-        alert( window.innerWidth, window.innerHeight )
         window.addEventListener('resize', () => {
             this.sizes.width = window.innerWidth
             this.sizes.height = window.innerHeight
