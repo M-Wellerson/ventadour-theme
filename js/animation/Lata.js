3f38f1315f2f5ec05ee57b944511ca5cac9d7c8d
brunocriacoes <br.rafael@outlook.com>
2022-03-11 11:04:20 -0300

    fix

--- js/animation/Lata.js
+++ js/animation/Lata.js
@@ -156,15 +156,17 @@ export default class Lata {
     }
 
     resize() {
-        window.addEventListener('resize', () => {
-            this.sizes.width = window.innerWidth
-            this.sizes.height = this.proporcional(window.innerWidth) // window.innerHeight
-            alert(this.sizes.width, this.sizes.height)
-            this.camera.aspect = this.sizes.width / this.sizes.height
-            this.camera.updateProjectionMatrix()
-            this.renderer.setSize(this.sizes.width, this.sizes.height)
-            this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-        })
+        if(window.innerWidth < 700) {
+            window.addEventListener('resize', () => {
+                this.sizes.width = window.innerWidth
+                this.sizes.height = this.proporcional(window.innerWidth) // window.innerHeight
+                alert(this.sizes.width, this.sizes.height)
+                this.camera.aspect = this.sizes.width / this.sizes.height
+                this.camera.updateProjectionMatrix()
+                this.renderer.setSize(this.sizes.width, this.sizes.height)
+                this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+            })
+        }
     }
 
     create_camera() {
